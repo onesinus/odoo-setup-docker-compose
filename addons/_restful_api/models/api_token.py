5a7d4df1ab1f0b0820f2494a4087266541db66af
Onesinus SPT <onesinus@delman.io>
2023-10-12 17:58:18 +0700

    Renegerate token and validation to ensure each user only has one token

--- addons/_restful_api/models/api_token.py
+++ addons/_restful_api/models/api_token.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class ApiToken(models.Model):
     _name = 'api.token'
@@ -8,3 +9,13 @@ class ApiToken(models.Model):
 
     def _generate_token(self):
         return self.user_id.generate_api_token()
+
+    def regenerate_token(self):
+        self.api_token = self.user_id.generate_api_token()
+
+    @api.constrains('user_id')
+    def _check_unique_user_id(self):
+        for token in self:
+            existing_tokens = self.env['api.token'].search([('user_id', '=', token.user_id.id)])
+            if len(existing_tokens) > 1:
+                raise ValidationError('Each user can only have one API token.')
